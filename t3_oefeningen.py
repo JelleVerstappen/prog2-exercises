5f4636577e1f91829cdf7ecd35ed9e59034fea75
Jelle <jelle.verstappen@telenet.be>
2021-04-21 08:46:36 +0200

    alle oef bijgewerkt

--- t3_oefeningen.py
+++ t3_oefeningen.py
@@ -12,18 +12,28 @@ def bmi(gewicht, lengte):
     return resultaat
 
 def omtrek_cirkel(r):
+    if r <= 0:
+        return -1
     resultaat = 2 * r * pi
     return resultaat
 
 def oppervlakte_cirkel(r):
+    if r <= 0:
+        return -1
     resultaat = (r **2) * pi
     return resultaat
 
 def oppervlakte_vierkant(zijde):
+    if zijde <= 0:
+        return -1
     resultaat = zijde **2
     return resultaat
 
 def volume_cilinder(r, h):
+    if r <= 0:
+        return -1
+    if h <= 0:
+        return -1
     resultaat = oppervlakte_cirkel(r) * h
     return resultaat
 
